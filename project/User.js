296ad47c936ac1005cc7fd44cfdba327e4e5ae18
Darya Wolfson <w0lfs0hnds@gmail.com>
2020-07-09 20:25:34 +0300

    endpoints added

---
+++ project/User.js
@@ -0,0 +1,11 @@
+/*const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+  
+
+const userScheme = new Schema({
+    id: String,
+    firstName: String,
+    lastName: String,
+    birthDate: Number,
+    marks:Array
+});*/
